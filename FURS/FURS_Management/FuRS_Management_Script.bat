50d4026bfc8203088646478f6467e84cd695ac06
singhraj82 <31572481+singhraj82@users.noreply.github.com>
2017-12-04 15:12:09 -0800

    Update FuRS_Management_Script.bat

--- FURS/FURS_Management/FuRS_Management_Script.bat
+++ FURS/FURS_Management/FuRS_Management_Script.bat
@@ -1,8 +1,8 @@
 rem Qmake the project file to generate make files.
 %QTDIR%\bin\qmake.exe FURS_Management.pro
 rem Compile the project.
-%QTDIR%\..\..\Tools\QtCreator\bin\jom.exe -f Makefile.Debug
+%QTDIR%\..\..\Tools\QtCreator\bin\jom.exe -f Makefile.Release
 rem Get all the required dependencies.
-%QTDIR%\bin\windeployqt.exe debug\FURS_Management.exe
+%QTDIR%\bin\windeployqt.exe release\FURS_Management.exe
 rem Create folder that will keep letters.
 mkdir ..\letters
